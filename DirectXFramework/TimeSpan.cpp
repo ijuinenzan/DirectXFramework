813fa5e83554532e663cf225d68847b7784f4593
Lê Phương Khanh <khanhlp315@gmail.com>
2017-09-24 16:35:06 +0700

    Implement Timespan

--- DirectXFramework/TimeSpan.cpp
+++ DirectXFramework/TimeSpan.cpp
@@ -8,22 +8,27 @@ TimeSpan::TimeSpan ( )
 
 TimeSpan::TimeSpan ( UINT64 ticks )
 {
+	_ticks = ticks;
 }
 
 float TimeSpan::getMillisecond ( ) const
 {
+	return _ticks / TicksPerMillisecond;
 }
 
 UINT64 TimeSpan::getTicks ( ) const
 {
+	return _ticks;
 }
 
 TimeSpan TimeSpan::operator+ ( TimeSpan timeSpan ) const
 {
+	return TimeSpan(this->_ticks + timeSpan._ticks);
 }
 
 TimeSpan TimeSpan::operator+ ( UINT64 ticks ) const
 {
+	return TimeSpan(this->_ticks + ticks);
 }
 
 
