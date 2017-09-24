7614009cb47a49af2e6a66eabcc79b45934fc905
Khanh dep trai <ijuinenzan@yahoo.com>
2017-09-24 16:35:26 +0700

    Implement GameTime

--- DirectXFramework/GameTime.cpp
+++ DirectXFramework/GameTime.cpp
@@ -13,18 +13,47 @@ void GameTime::release ( )
 
 GameTime* GameTime::getInstance ( )
 {
+	if (_instance != NULL)
+	{
+		_instance = new GameTime();
+	}
+	return _instance;
 }
 
 void GameTime::init ( )
 {
+	_startTick = 0;
+	_lastTick = 0;
+	_currentTick = 0;
+
+	QueryPerformanceFrequency(&_query);
+	_frequencyQuery = float(_query.QuadPart) / 10000000;
+
+	QueryPerformanceCounter(&_query);
+	_lastTick = _query.QuadPart;
+	_startTick = _lastTick;
+	_totalGameTime = (TimeSpan)0;
 }
 
 void GameTime::resetLastTick ( )
 {
+	_lastTick = 0;
+	_currentTick = 0;
+	_totalGameTime = (TimeSpan)0;
 }
 
 void GameTime::updateGameTime ( )
 {
+	QueryPerformanceCounter(&_query);
+
+	_currentTick = _query.QuadPart;
+
+	_elapsedGameTime = float(_currentTick - _lastTick) / _frequencyQuery;
+
+
+	_totalGameTime = _totalGameTime + _elapsedGameTime;
+
+	_lastTick = _currentTick;
 }
 
 float GameTime::getElapsedGameTime ( ) const
